cdb90482bf932c7928fbf908cfc9893f2536dcb9
IgnacioFRT <ignacio.fernandez@alu.frt.utn.edu.ar>
2026-04-19 17:37:05 -0300

    Update perfil_dinamico.py

--- ems.V2/views/perfil_dinamico.py
+++ ems.V2/views/perfil_dinamico.py
@@ -7,6 +7,13 @@ from views.historico import get_historical_data
 
 
 def render_perfil_dinamico():
+
+    st.markdown(
+        """
+        <div style="margin-top: 12px;"></div>
+        """,
+        unsafe_allow_html=True
+    )
     try:
         with st.spinner("Procesando perfiles de carga interactivos... ⏳"):
             df = get_historical_data().copy()
