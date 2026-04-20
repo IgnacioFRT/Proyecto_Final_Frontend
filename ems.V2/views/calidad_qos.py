29956bee6c47ec6573843ad1b08f1988fa82984e
IgnacioFRT <ignacio.fernandez@alu.frt.utn.edu.ar>
2026-04-20 20:29:34 -0300

    Update calidad_qos.py

--- ems.V2/views/calidad_qos.py
+++ ems.V2/views/calidad_qos.py
@@ -249,7 +249,7 @@ def render_calidad_qos():
                 xaxis=dict(
                     rangeslider=dict(visible=True),
                     type="date"
-                    range=[df_filtrado.index.min(), df_filtadro.index.max()]
+                    range=[df_filtrado.index.min(), df_filtrado.index.max()]
                 ),
                 legend=dict(
                     orientation="h",
