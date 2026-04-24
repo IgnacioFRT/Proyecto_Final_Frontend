e50bca127c14368b037c4d4cda9b36f60a9cacc0
IgnacioFRT <ignacio.fernandez@alu.frt.utn.edu.ar>
2026-04-24 18:41:29 -0300

    Update styles.py

--- ems.V2/styles.py
+++ ems.V2/styles.py
@@ -117,8 +117,8 @@ def load_global_styles():
             border-radius: 8px;
             height: 42px;
             font-weight: 500;
-            background-color: #e7f0ff;
-            color: #0d47a1;
+            background-color: #2c5282;
+            color: white;
             border: 1px solid #2c5282;
             transition: 0.2s;
         }
